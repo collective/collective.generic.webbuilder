5465333c1a36b19f6bb20e86c5328b8f4d967569
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-12-10 17:45:34 +0100

    switch close/open icon on accordion

--- src/collective/generic/webbuilder/templates/static_dev/theme.js
+++ src/collective/generic/webbuilder/templates/static_dev/theme.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('a.option-desc-url').html('<i class="fa fa-external-link"></i>');
+    $(document).on('click', 'a[data-toggle="collapse"]', function(){
+        var $close = $(this).find('i.fa-caret-square-o-right');
+        var $open = $(this).find('i.fa-caret-square-o-down');
+        if ($close.length == 1){
+            $close.replaceWith('<i class="fa fa-caret-square-o-down"></i>');
+        }else if ($open.length == 1){
+            $open.replaceWith('<i class="fa fa-caret-square-o-right"></i>');
+        }
+    });
 });
