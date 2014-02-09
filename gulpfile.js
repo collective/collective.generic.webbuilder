eec794e37c554a914104a4c85a90b8ebe158c767
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-02-09 15:19:33 +0100

    useless dep

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
 var less = require('gulp-less');
